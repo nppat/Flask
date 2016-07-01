4f05c84c9126e7da2ac003d8c85d9310e2738f4b
Patrick Adamson <nppat@hotmail.com>
2016-07-01 17:54:51 -0500

    updated readme, server.py,style.css, index.html

--- Assignments/ninja_gold/readme.md
+++ Assignments/ninja_gold/readme.md
@@ -1,12 +1,13 @@
 Ninja Gold
 
 Front-end
-1) Create 4 forms (farm,cave,house,casino) []
-2) Forms each have a hidden value
-3) Create textarea to print off the results of processing the gold []
-4) Show user gold at top of page
+- 1) Create 4 forms (farm,cave,house,casino) [x]
+- 2) Forms each have a hidden value [x]
+- 3) Create area to print off the results of processing the gold [x]
+- 4) Show user gold at top of page [x]
 
 Back-end
-1) create 2 routes ('/','/process_money') []
-2) Create logic in /process_money to process the gold []
-3) Send out gold count to displays on index.html []
+- 1) create 2 routes ('/','/process_money') [x]
+- 2) Create logic in /process_money to process the gold [x]
+- 3) Send out gold count to displays on index.html [x]
+- 4) Set up reset button [x]
--- Assignments/ninja_gold/server.py
+++ Assignments/ninja_gold/server.py
@@ -2,19 +2,42 @@ from flask import Flask, request, redirect, render_template, session, flash
 app = Flask(__name__)
 app.secret_key = "YOLOSwagLitFam"  #set the session secret key to someting awesome
 
+# Build class Gold to keep track of gold count initialize at zero
+class Gold(object):
+	def __init__(self):
+		self.count = 0
+
+gold = Gold() # set instance of Gold()
+
 #set index.html route
 @app.route('/')
 def index():
-	gold = 0
-	session['gold_count'] = gold
-	print session['gold_count']
+	session['gold_count'] = gold.count
+	print "GOLD INDEX IS BOOMING", session['gold_count']
 	return render_template('index.html')
 
+#set up '/process_money route and function'
 @app.route('/process_money', methods=['POST'])
 def process_money():
-	name = request.form['value'] #grab num inputted and covert to int to check against ran number
+	name = request.form['action']
 	if(name == 'hidden_farm'):
-		print "FArm"
+		gold.count += 15
+		flash("All that farming earned you 15 gold.", 'gold')
+	elif name == "hidden_cave":
+		gold.count += 7
+		flash('Explore cave, get 7 gold', 'gold')
+	elif name == "hidden_house":
+		gold.count += 2
+		flash('That house earned you 2 gold','gold')
+	elif name == "hidden_casino":
+		gold.count += 50
+		flash("Hit that craps table!", "gold")
 	return redirect('/')
-
+#set up '/reset' route and function
+@app.route('/reset')
+def reset():
+	print "No more gold :(" # prints to ensure button is working
+	session.pop('gold_count') # clear out the session data for "gold_count"
+	gold.count = 0 #reset gold to 0 count
+	return redirect('/')
 app.run(debug=True) #run the server
